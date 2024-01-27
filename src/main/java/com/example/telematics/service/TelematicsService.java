cbf18805a6c24a7e8e91c3a3db690ea73bdfb954
Apurv <apurv.jaiswal@walmart.com>
2024-01-28 02:05:00 +0530

    removed unused property

--- src/main/java/com/example/telematics/service/TelematicsService.java
+++ src/main/java/com/example/telematics/service/TelematicsService.java
@@ -3,16 +3,12 @@ package com.example.telematics.service;
 import com.example.telematics.model.TelematicsData;
 import com.example.telematics.repository.TelematicsDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TelematicsService {
-
-    @Value("${telematics.ingestion.frequency}")
-    private int ingestionFrequency;
 
     @Autowired
     private TelematicsDataRepository telematicsDataRepository;
